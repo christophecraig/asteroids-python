fd853629036d0769e27c1abbbe3988d3f4864a80
christophecraig <christophe.craig@viacesi.fr>
2024-11-07 23:58:26 +0100

    finish chapter 2 : group sprites, moving player

--- main.py
+++ main.py
@@ -3,16 +3,26 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 
 def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    updatables = pygame.sprite.Group()
+    drawables = pygame.sprite.Group()
+    Player.containers = (updatables, drawables)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        for drawable in drawables:
+            drawable.draw(screen)
+        dt = clock.tick(60) / 1000.0
+        for updatable in updatables:
+            updatable.update(dt)
         pygame.display.flip()
 
 if __name__ == "__main__":
